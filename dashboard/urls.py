14d9703e2402a7803c2340a2b9bb3e2992f413fa
Alisson Santos <cnctalissonsantos@gmail.com>
2026-01-30 00:52:17 -0300

    Change dashboard login page design

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, reverse
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 app_name = "dashboard"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", LoginView.as_view(template_name="dashboard/login.html", next_page="dashboard:index"), name="login"),
+    path("login/", views.check_login, name="login"),
     path("logout/", LogoutView.as_view(next_page="dashboard:login"), name="logout"),
 ]
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,18 @@
-from django.contrib.auth.views import login_required
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
 from django.urls import reverse
-from django.views import generic
-
-from .forms import LoginForm
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView, login_required
 
 # Create your views here.
+
+def check_login(request):
+
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("dashboard:index"))
+
+    else:
+        return LoginView.as_view(template_name="dashboard/login.html", next_page="dashboard:index")(request)
+
 
 @login_required(login_url="dashboard:login")
 def index(request):
