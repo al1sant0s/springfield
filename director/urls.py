7257adae2f040093ea5c47b83cceb58373e8b620
Alisson Santos <cnctalissonsantos@gmail.com>
2026-05-09 13:07:38 -0300

    Include missing url for ios devices

--- director/urls.py
+++ director/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "director"
 urlpatterns = [
     path("api/<str:platform>/getDirectionByPackage", views.getDirectionByPackage, name="getDirectionByPackage"),
+    path("api/<str:platform>/getDirectionByBundle", views.getDirectionByPackage, name="getDirectionByBundle")
 ]
